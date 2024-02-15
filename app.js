e50a36707ba443aeca1f876f194d46231f7c3da9
Michael J. Klein <lambdamichael@gmail.com>
2024-02-15 14:54:33 -0500

    wip recursion, updating to api that skips ggregation_object, testing minimal examples

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
 import { Noir } from '@noir-lang/noir_js';
 import rec_test_circuit from './rec_test_circuit/target/rec_test_circuit.json';
+import input_json from './rec_test_circuit/Prover.json';
 
 document.addEventListener('DOMContentLoaded', async () => {
   const backend = new BarretenbergBackend(rec_test_circuit);
   const noir = new Noir(rec_test_circuit, backend);
-  const input = { x: 1, y: 2 };
+
+  // const input = { x: 1, y: 2 };
+  // (toml2json --pretty Prover.toml > Prover.json)
+  const input = input_json; // parse(readFileSync('./rec_test_circuit/Prover.toml', 'utf-8'));
+  console.log('input', input);
+
   display('logs', 'Generating proof... ⌛');
   const proof = await noir.generateFinalProof(input);
   display('logs', 'Generating proof... ✅');
@@ -20,4 +26,5 @@ function display(container, msg) {
   const p = document.createElement('p');
   p.textContent = msg;
   c.appendChild(p);
+  console.log(container, msg);
 }
